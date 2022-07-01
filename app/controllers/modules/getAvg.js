ea2627230bd49597a704eeaf1c1b7e9babf4d573
CuberLuo <1501858187@qq.com>
2022-07-01 23:18:29 +0800

    平均分四舍五入

--- app/controllers/modules/getAvg.js
+++ app/controllers/modules/getAvg.js
@@ -3,17 +3,18 @@ module.exports = {
   getAvg: (request, response) => {
     const Cno = request.body.Cno
     const Year = request.body.Year
-    console.log(Cno, Year)
+    // console.log(Cno, Year)
     const sql = `
 		SELECT AVG_SCORE 
 		FROM ljt_average_score
 		WHERE Cno=? and ScoreYear=?
 		`
     connection.query(sql, [Cno, Year], function (results) {
+      const data = parseFloat(results[0].AVG_SCORE)
       // console.log(results)
       response.send({
         code: 10000,
-        AVG_SCORE: results[0].AVG_SCORE
+        AVG_SCORE: Math.round(data * 10) / 10
       })
     })
   }
